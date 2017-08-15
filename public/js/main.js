be790416d73bb31e89524a0dacda03a1e8968b98
Mary Yen <mymaryyn@gmail.com>
2017-08-15 18:24:27 -0400

    delete it

--- public/js/main.js
+++ public/js/main.js
@@ -24,4 +24,60 @@ function initialize_gmaps() {
 
 $(document).ready(function() {
     initialize_gmaps();
+
+
+	hotels.forEach((hotel) => {
+		$(`<option value=${hotel.name} data-place=${hotel.place.location}> ${hotel.name} </option>`).appendTo('#hotel-choices');
+	})
+	restaurants.forEach((restaurant) => {
+		$(`<option value=${restaurant.name} data-place=${restaurant.place.location}> ${restaurant.name} </option>`).appendTo('#restaurant-choices');
+	})
+	activities.forEach((activity) => {
+		$(`<option value=${activity.name} data-place=${activity.place.location}> ${activity.name} </option>`).appendTo('#activity-choices');
+	})
+
+	// attach listeners to add buttons
+	$('#hotel-add').on('click', 'button', function () {
+		let nod = $(this).parent().find(':selected');
+		addToItinMap(nod, '#hotel-list');
+	})
+
+	$('#restaurant-add').on('click', 'button', function () {
+		let nod = $(this).parent().find(':selected');
+		addToItinMap(nod, '#restaurant-list');
+	})
+
+	$('#activity-add').on('click', 'button', function () {
+		let nod = $(this).parent().find(':selected');
+		addToItinMap(nod, '#activity-list');
+	})
+
+	function addToItinMap(nod, listId) {
+		$(`<div class="itinerary-item">
+			<span class="title">${nod.html()}</span>
+			<button class="btn btn-xs btn-danger remove btn-circle">x</button>
+			</div>`)
+			.appendTo(listId);
+
+		var loc = nod.get(0).dataset.place.split(',');
+		//initialize latlong object
+		var myLatlng = new google.maps.LatLng(loc[0], loc[1])
+
+		// Add the marker to the map
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			title: nod.html(),
+			visible: true
+		});
+		// Add the marker to the map by calling setMap()
+		marker.setMap(map);
+		//remove itenary
+		$('.itinerary-item > .remove').on('click', function () {
+			$(this).parent().remove();
+			marker.setMap(null);
+		})
+	}
+
 });
+
+
--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 // This file should contain all the record creation needed to seed the database with its default values.
-// The data can then be loaded with the node seed.js 
+// The data can then be loaded with the node seed.js
 
 var Promise = require('bluebird');
 var db = require('./models');
@@ -9,71 +9,80 @@ var Restaurant = require('./models/restaurant');
 var Activity = require('./models/activity');
 
 var data = {
-  hotel: [
-    {name: "Wyndham Grand Chicago Riverfront", place: {address: "71 E Upper Wacker Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8884073, -87.6293817]}, num_stars: 4, amenities: "Pool, Free Wi-Fi" },
-    {name: "Courtyard by Mariott", place: {address: "30 E Hubbard St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8884073, -87.6293817]}, num_stars: 4.5, amenities: "Free Wi-Fi" },
-    {name: "Aloft Chicago City Center", place: {address: "515 N Clark St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.889857, -87.6321275]}, num_stars: 4.5, amenities: "24 hour Gym, Paid Wi-Fi" },
-    {name: "Fairmount Chicago Millenium Park", place: {address: "200 N Columbus Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8873346, -87.6290507]}, num_stars: 4, amenities: "Free Wi-Fi" },
-    {name: "Hyatt Regency Chicago", place: {address: "151 E Upper Wacker Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8881029, -87.6264413]}, num_stars: 3.5, amenities: "24 hour Gym" },
-    {name: "Sheraton Grand Chicago", place: {address: "301 E North Water St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8883059, -87.6260713]}, num_stars: 3.5, amenities: "24 hour Gym" },
-    {name: "Kimpton Hotel Allegro", place: {address: "171 W Randolph St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8861737, -87.6339368]}, num_stars: 3.5, amenities: "Free Wi-Fi" },
-    {name: "Residence Inn By Mariott", place: {address: "11 S LaSalle St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8843845, -87.634366]}, num_stars: 3.5, amenities: "Paid Wi-Fi, Dogs Allowed" },
-    {name: "The Alise Chicago", place: {address: "1 W Washington St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8843845, -87.634366]}, num_stars: 3.5, amenities: "Paid Wi-Fi" },
-    {name: "Palmer House", place: {address: "17 E Monroe St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8843845, -87.634366]}, num_stars: 2, amenities: "Accepts Credit Cards" },
-    {name: "Four Seasons", place: {address: "120 E Delaware Pl", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8974821, -87.6307182]}, num_stars: 4, amenities: "Paid Wi-Fi" },
-    {name: "Millenium Knickerbocker", place: {address: "163 E Walton Pl", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8974821, -87.6307182]}, num_stars: 3.5, amenities: "Pool, 24 hour Gym, Paid Wi-Fi" },
-    {name: "The Ritz-Carlton Chicago", place: {address: "Water Tower Place", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8974821, -87.6307182]}, num_stars: 3.5, amenities: "24 hour Gym, Paid Wi-Fi" },
-    {name: "Waldorf Astoria Chicago", place: {address: "11 E Walton St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8997095, -87.6296546]}, num_stars: 3.5, amenities: "Free Wif-Fi" },
-    {name: "Hilton Chicago", place: {address: "720 S Michigan Ave", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8813272, -87.640199]}, num_stars: 4, amenities: "Free Wif-Fi" }
+  hotels: [
+    {name: "Andaz Wall Street", place: {address: "75 Wall St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705137, -74.007624]}, num_stars: 4, amenities: "Pool, Free Wi-Fi" },
+    {name: "Hotel Mulberry", place: {address: "52 Mulberry St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715317, -73.999542]}, num_stars: 4.5, amenities: "Free Wi-Fi" },
+    {name: "The Ritz-Carlton New York, Battery Park", place: {address: "Two West Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705417, -74.017241]}, num_stars: 4.5, amenities: "24 hour Gym, Paid Wi-Fi" },
+    {name: "Wall Street Inn", place: {address: "9 S William St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.704581, -74.010273]}, num_stars: 4, amenities: "Free Wi-Fi" },
+    {name: "Smyth TriBeCa", place: {address: "85 West Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715076, -74.009301]}, num_stars: 3.5, amenities: "24 hour Gym" },
+    {name: "Double Tree", place: {address: "8 Stone St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.703961, -74.012350]}, num_stars: 3.5, amenities: "24 hour Gym" },
+    {name: "Hotel 91", place: {address: "91 E Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.713427, -73.993624]}, num_stars: 3.5, amenities: "Free Wi-Fi" },
+    {name: "Conrad New York Hotel", place: {address: "102 N End Ave", city: "New York", state: "NY", phone: "123-456-7890", location: [40.714985, -74.015841]}, num_stars: 3.5, amenities: "Paid Wi-Fi, Dogs Allowed" },
+    {name: "Millenium Hilton Hotel", place: {address: "55 Church St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.711597, -74.010392]}, num_stars: 3.5, amenities: "Paid Wi-Fi" },
+    {name: "US Pacific Hotel", place: {address: "106 Bowery", city: "New York", state: "NY", phone: "123-456-7890", location: [40.717873, -73.995231]}, num_stars: 2, amenities: "Accepts Credit Cards" },
+    {name: "Gild Hall, a Thompson Hotel", place: {address: "15 Gold Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.707894, -74.007108]}, num_stars: 4, amenities: "Paid Wi-Fi" },
+    {name: "W New York", place: {address: "123 Washington Street", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709102, -74.013997]}, num_stars: 3.5, amenities: "Pool, 24 hour Gym, Paid Wi-Fi" },
+    {name: "New York Marriott Downtown", place: {address: "85 W St at Albany St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.731131, -73.989568]}, num_stars: 3.5, amenities: "24 hour Gym, Paid Wi-Fi" },
+    {name: "Cosmopolitan Hotel", place: {address: "95 W Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715681, -74.008922]}, num_stars: 3.5, amenities: "Free Wif-Fi" },
+    {name: "Club Quarters", place: {address: "140 Washington St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709630, -74.013940]}, num_stars: 4, amenities: "Free Wif-Fi" }
   ],
-  restaurant: [
-    {name: "Alinea", place: {address: "W Willow St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.9134555, -87.6503527]}, cuisine: "New American", price: 4},
-    {name: "The Purple Pig", place: {address: "The Shops at North Bridge", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8912974, -87.6270254]}, cuisine: "New American", price: 3},
-    {name: "Velvet Taco", place: {address: "1110 N State St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.9022287,-87.6307428]}, cuisine: "Mexican", price: 3},
-    {name: "Seoul Taco", place: {address: "738 N Clark St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [40.714230, -73.998509]}, cuisine: "Korean/Mexican", price: 2},
-    {name: "Cafe Babareeba", place: {address: "2024 N Halsted St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.9189836,-87.6508765]}, cuisine: "Spanish", price: 4},
-    {name: "Bongo Room", place: {address: "1152 S Wabash Ave", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.919094,-87.7187281]}, cuisine: "American", price: 4},
-    {name: "BIG & little's", place: {address: "860 N Orleans St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.89835,-87.6396717]}, cuisine: "Southern", price: 3},
-    {name: "Giordano’s", place: {address: "730 N Rush St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8984125,-87.6725025]}, cuisine: "Italian", price: 2},
-    {name: "Medici on 57th", place: {address: "1327 E 57th St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8392942,-87.6982452]}, cuisine: "American", price: 2},
-    {name: "Arbella", place: {address: "112 W Grand Ave", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.891873,-87.6339857]}, cuisine: "Cocktails", price: 3},
-    {name: "Green Apple PHOever", place: {address: "105 W Madison St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8817877,-87.6333197]}, cuisine: "Vietnamese", price: 4},
-    {name: "River Roast", place: {address: "315 N LaSalle Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8879907,-87.6344459]}, cuisine: "British", price: 2},
-    {name: "Osaka Express", place: {address: "400 Michigan Avenue", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8769669,-87.6267487]}, cuisine: "Japanese", price: 3},
-    {name: "Artisan Cellar", place: {address: "theMART", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8881524,-87.6372443]}, cuisine: "Cheese", price: 4},
-    {name: "Ramen-san", place: {address: "59 W Hubbard St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8898032,-87.6324809]}, cuisine: "Vietnamese", price: 1}
+  restaurants: [
+    {name: "Bouley", place: {address: "75 Wall St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705137, -74.013940]}, cuisine: "French", price: 4},
+    {name: "Marc Forgione", place: {address: "134 Reade St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.716526, -74.009567]}, cuisine: "Seafood", price: 3},
+    {name: "Tamarind", place: {address: "99 Hudson St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.718977, -74.008929]}, cuisine: "Indian", price: 3},
+    {name: "Hop Lee Restaurant", place: {address: "16 Mott St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.714230, -73.998509]}, cuisine: "Chinese", price: 2},
+    {name: "Jungsik", place: {address: "2 Harrison St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.718679, -74.008900]}, cuisine: "Korean", price: 4},
+    {name: "The Capital Grille", place: {address: "120 Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.708475, -74.010846]}, cuisine: "Steakhouses, American", price: 4},
+    {name: "Pylos", place: {address: "128 E 7th St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.726096, -73.984152]}, cuisine: "Greek", price: 3},
+    {name: "Joe's Shanghai", place: {address: "9 Pell St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.714601, -73.997761]}, cuisine: "Shanghainese, Dim Sum", price: 2},
+    {name: "Cafe Katja", place: {address: "79 Orchard St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.717719, -73.990565]}, cuisine: "German, Austrian", price: 2},
+    {name: "Rosanjin", place: {address: "141 Duane St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.716403, -74.007724]}, cuisine: "Japanese", price: 3},
+    {name: "Kittichai", place: {address: "60 Thompson St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.724014, -74.003242]}, cuisine: "Thai", price: 4},
+    {name: "Bianca Restaurant", place: {address: "5 Bleecker St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.725495, -73.992662]}, cuisine: "Italian", price: 2},
+    {name: "Rayuela", place: {address: "165 Allen St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.721266, -73.989756]}, cuisine: "Spanish, Latin American", price: 3},
+    {name: "Mas Farmhouse", place: {address: "39 Downing St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.729269, -74.003875]}, cuisine: "New American, French", price: 4},
+    {name: "Xe Lua", place: {address: "86 Mulberry St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.716544, -73.998626]}, cuisine: "Vietnamese", price: 1}
   ],
-  activity: [
-    {name: "Shedd Aquarium", place: {address: "1200 S Lake Shore Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8675726, -87.614038]}, age_range: "All" },
-    {name: "Adler Planetarium", place: {address: "1300 S Lake Shore Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.866337, -87.6089769]}, age_range: "All" },
-    {name: "Field Museum", place: {address: "1400 S Lake Shore Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.866265, -87.6191745]}, age_range: "All" },
-    {name: "Museum of Science and Industry", place: {address: "5700 S Lake Shore Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.7905726, -87.5830659]}, age_range: "All" },
-    {name: "Art Institute of Chicago", place: {address: "111 S Michigan Ave", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8795885, -87.6259073]}, age_range: "All" },
-    {name: "Cloud Gate", place: {address: "Millennium Park", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8826612, -87.6254979]}, age_range: "All" },
-    {name: "Harold Washington Library", place: {address: "400 S State St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8763008, -87.6304219]}, age_range: "All" },
-    {name: "Jackson Park", place: {address: "1793 E Hayes Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.7798608, -87.5827403]}, age_range: "All" },
-    {name: "John Hancock Tower", place: {address: "875 N Michigan Ave", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.8988124, -87.6252732]}, age_range: "All" },
-    {name: "Wrigley Field", place: {address: "1060 W Addison St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.9484424, -87.6575267]}, age_range: "All" },
-    {name: "57th St. Bookstore", place: {address: "1301 E 57th St", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.7911832, -87.5968324]}, age_range: "All" },
-    {name: "Montrose Beach", place: {address: "4400 N Lake Shore Dr", city: "Chicago", state: "IL", phone: "123-456-7890", location: [41.9655364, -87.6385693]}, age_range: "All" }
+  activities: [
+    {name: "Mahayana Temple Buddhist Association", place: {address: "133 Canal St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.716291, -73.995315]}, age_range: "All" },
+    {name: "South Street Seaport", place: {address: "19 Fulton St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.707119, -74.003602]}, age_range: "All" },
+    {name: "Ground Zero", place: {address: "1 Liberty Plz Lbby 2", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709329, -74.013120]}, age_range: "All" },
+    {name: "National September 11th Memorial & Museum", place: {address: "1 Albany St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.709189, -74.013157]}, age_range: "All" },
+    {name: "Battery Park", place: {address: "State St & Battery Pl", city: "New York", state: "NY", phone: "123-456-7890", location: [40.704531, -74.014334]}, age_range: "All" },
+    {name: "Staten Island Ferry Whitehall Terminal", place: {address: "4 S St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.729125, -73.989650]}, age_range: "All" },
+    {name: "Chinatown Ice Cream Factory", place: {address: "65 Bayard St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.715323, -73.998190]}, age_range: "All" },
+    {name: "Blue Man Group", place: {address: "434 Lafayette St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.729373, -73.992104]}, age_range: "All" },
+    {name: "Scott's Pizza Tours", place: {address: "244 5th Ave", city: "New York", state: "NY", phone: "123-456-7890", location: [40.744610, -73.987708]}, age_range: "All" },
+    {name: "Apple Store", place: {address: "103 Prince St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.724918, -73.999144]}, age_range: "All" },
+    {name: "Brooklyn Bridge Park", place: {address: "Pier 1", city: "New York", state: "NY", phone: "123-456-7890", location: [40.702225, -73.996563]}, age_range: "All" },
+    {name: "Ellis Island Immigration Museum", place: {address: "Ellis Island", city: "New York", state: "NY", phone: "123-456-7890", location: [40.699297, -74.040317]}, age_range: "All" },
+    {name: "Washington Square Park", place: {address: "1 Washington Sq E", city: "New York", state: "NY", phone: "123-456-7890", location: [40.732204, -73.998649]}, age_range: "All" },
+    {name: "Union Square Holiday Market", place: {address: "Union Sq & W 14th St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.733615, -73.987995]}, age_range: "All" },
+    {name: "Strand Bookstore", place: {address: "828 Broadway", city: "New York", state: "NY", phone: "123-456-7890", location: [40.733274, -73.990870]}, age_range: "All" }
   ]
 };
 
 db.sync({force: true})
 .then(function () {
   console.log("Dropped old data, now inserting data");
-  return Promise.map(Object.keys(data), function (name) {
-    return Promise.map(data[name], function (item) {
-      return db.model(name)
-      .create(item, {
-        include: [Place]
-      });
-    });
+  const creatingHotels = Promise.map(data.hotels, function (hotel) {
+    return Hotel.create(hotel, { include: [Place] });
   });
+  const creatingRestaurants = Promise.map(data.restaurants, function (restaurant) {
+    return Restaurant.create(restaurant, { include: [Place] });
+  });
+  const creatingActivities = Promise.map(data.activities, function (activity) {
+    return Activity.create(activity, { include: [Place] });
+  });
+  return Promise.all([creatingHotels, creatingRestaurants, creatingActivities]);
 })
 .then(function () {
-  console.log("Finished inserting data (press ctrl-c to exit)");
+  console.log('Finished inserting data');
 })
 .catch(function (err) {
   console.error('There was totally a problem', err, err.stack);
+})
+.finally(function () {
+  db.close(); // creates but does not return a promise
+  return null; // stops bluebird from complaining about un-returned promise
 });
